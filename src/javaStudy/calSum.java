9f85f5f37458057c241c2b31042bbf0d7371ebd6
yejin5 <ohyejin1117@gmail.com>
2024-03-05 17:25:39 +0900

    add info

--- src/javaStudy/calSum.java
+++ src/javaStudy/calSum.java
@@ -3,6 +3,15 @@ package javaStudy;
 import java.util.Scanner;
 
 public class calSum {
+
+    /*
+    * N개의 숫자가 공백 없이 써 있다.
+    * 이 숫자를 모두 합해 출력하는 프로그램을 작성하시오.
+    *
+    * 1번째 줄에 숫자의 개수 N(1<=N<=100)
+    * 2번째 줄에 숫자 N개가 공백 없이 주어진다.
+    * */
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
--- src/javaStudy/sumArr.java
+++ src/javaStudy/sumArr.java
@@ -6,6 +6,16 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class sumArr {
+
+    /*
+    * 수 N개가 주어졌을 때 i 번째 수에서 j 번째 수까지의 합을 구하는 프로그램을 작성하시오.
+    *
+    * 1번째 줄에 수의 개수 N(1<=N<=100000), 합을 구해야 하는 횟수 M(1<=M<=100000)
+    * 2번째 줄에 N개의 수가 주어진다.
+    * 각 수는 1000보다 작거나 같은 자연수이다.
+    * 3번째 줄부터는 M개의 줄에 합을 구해야 하는 구간 i와 j가 주어진다.
+    * */
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
